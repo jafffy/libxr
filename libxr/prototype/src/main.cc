f61115a558cd89cf8e0f91206c5b3a716dfe0f84
Jaewon Choi <jaewon.james.choi@gmail.com>
2017-12-16 02:30:15 +0900

    Add glew

--- libxr/prototype/src/main.cc
+++ libxr/prototype/src/main.cc
@@ -1,5 +1,9 @@
-#include <GLFW/glfw3.h>
+#include <cstdio>
+
 #include <Windows.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
@@ -12,11 +16,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
         return -1;
     }
 
-    glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window);
+
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+		char buf[512];
+		sprintf_s(buf, "Error: %s\n", glewGetErrorString(err));
+		OutputDebugStringA(buf);
+		return -1;
+	}
+	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
 
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0, 0, 0.3, 0);
+        glClearColor(0, 0, 0.3f, 0);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
